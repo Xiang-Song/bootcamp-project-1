103c37094df67689352d046425d55bee977098f9
Xiang Song <himmelsong@gmail.com>
2020-03-26 10:12:43 -0700

    css details for projects

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
+$(document).ready(
+    function(){
+        $("#Py").animate({
+            width:"85%"
+        }, 3000);
+    });
+
 $(function(){
-    $("#Pyprogress").animate({
+    $("#sql").animate({
         width:"85%"
     }, 3000);
 });
 
+$(function(){
+    $("#html").animate({
+        width:"80%"
+    }, 3000);
+});
 
+$(function(){
+    $("#css").animate({
+        width:"35%"
+    }, 3000);
+});
+
+$(function(){
+    $("#js").animate({
+        width:"50%"
+    }, 3000);
+});
+
+$(function(){
+    $("#jq").animate({
+        width:"50%"
+    }, 3000);
+});
+$(function(){
+    $("#ag").animate({
+        width:"10%"
+    }, 3000);
+});
+
+$(function(){
+    $("#bs").animate({
+        width:"15%"
+    }, 3000);
+});
